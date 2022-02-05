a8dd61491a26c66c582fea650099e5edd8a2ae25
Srinivasan Ramjee <srinivasan.ramjee@gmail.com>
2022-02-04 20:32:33 -0500

    test

---
+++ src/main/java/com/ap/HashTest.java
@@ -0,0 +1,35 @@
+package com.ap;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.google.common.base.Strings;
+
+public class HashTest {
+	public static void main(String[] args) {
+		for (int i = 0; i < 3; i++)
+			System.out.println((int) (Math.random() * 2 + 1));
+
+		String[] strArray = new String[15];
+
+		for (int i = 0; i < strArray.length; i++)
+			strArray[i] = Double.toString(Math.random() + Math.random());
+
+		System.out.println(Integer.toBinaryString(15));
+
+		int n = 15;
+
+		for (String s : strArray) {
+			int hashCode = s.hashCode();
+			System.out.println(
+					s + "\t" + StringUtils.leftPad(Integer.toBinaryString(hashCode), 32, "0") + "\t" + (hashCode & n));
+		}
+		
+		System.out.println("");
+
+		for (String s : strArray) {
+			int hashCode = s.hashCode();
+			System.out.println(
+					s + "\t" + Strings.padStart(Integer.toBinaryString(hashCode), 32, '0') + "\t" + (hashCode & n));
+		}
+	}
+}
